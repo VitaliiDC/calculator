5d18353f02aa38a68ee9fd58dc1a2a522def3e6f
Vitalii Drobit <cdrobitvitalii@gmail.com>
2020-09-23 20:36:49 +0300

    Updates

---
+++ precache-manifest.1d26abfdad6034ca09d6ab216d348aac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5ab47eef0df9e47ed55d1964e4bd79b",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "c282379e82d9cf5b2f1e",
+    "url": "/calculator/static/css/main.35765152.chunk.css"
+  },
+  {
+    "revision": "7eb50ee11ddae803128e",
+    "url": "/calculator/static/js/2.f6fdace6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/calculator/static/js/2.f6fdace6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c282379e82d9cf5b2f1e",
+    "url": "/calculator/static/js/main.9eadbaf8.chunk.js"
+  },
+  {
+    "revision": "d3fc2a2896294ca2ab3f",
+    "url": "/calculator/static/js/runtime-main.99dd7910.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
